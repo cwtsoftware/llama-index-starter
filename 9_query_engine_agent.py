44aa24d1e063a7455125423928c45b79c6b787b6
moreno1123 <moreno.franjkovic@outlook.com>
2023-12-21 15:56:17 +0100

    agent prompt adjustment

--- 9_query_engine_agent.py
+++ 9_query_engine_agent.py
@@ -26,6 +26,7 @@ from llama_index import (
     load_index_from_storage,
 )
 
+# učitaj prvi indeks za godinu 2021
 try:
     storage_context = StorageContext.from_defaults(
         persist_dir="./storage/2021"
@@ -36,6 +37,16 @@ try:
 except:
     index_one_loaded = False
 
+if not index_one_loaded:
+    twentyone_docs = SimpleDirectoryReader(
+        input_files=["./godisnje-izvjesce-2021-CA.pdf"]
+    ).load_data()
+
+    twentyone_index = VectorStoreIndex.from_documents(twentyone_docs)
+
+    twentyone_index.storage_context.persist(persist_dir="./storage/2021")
+
+# učitaj drugi indeks za godinu 2022
 try:
     storage_context = StorageContext.from_defaults(
         persist_dir="./storage/2022"
@@ -45,15 +56,6 @@ try:
     index_two_loaded = True
 except:
     index_two_loaded = False
-
-if not index_one_loaded:
-    twentyone_docs = SimpleDirectoryReader(
-        input_files=["./godisnje-izvjesce-2021-CA.pdf"]
-    ).load_data()
-
-    twentyone_index = VectorStoreIndex.from_documents(twentyone_docs)
-
-    twentyone_index.storage_context.persist(persist_dir="./storage/2021")
 
 if not index_two_loaded:
     twentytwo_docs = SimpleDirectoryReader(
@@ -74,9 +76,9 @@ query_engine_tools = [
     QueryEngineTool(
         query_engine=twentyone_engine,
         metadata=ToolMetadata(
-            name="croatia_airlines_2021",
+            name="croatia_airlines_report_2021",
             description=(
-                "Provides information about Croatia Airlines financials for year 2021. "
+                "Provides information about Croatia Airlines bussines report for year 2021. "
                 "Use a detailed plain text question as input to the tool."
             ),
         ),
@@ -84,9 +86,9 @@ query_engine_tools = [
     QueryEngineTool(
         query_engine=twentytwo_engine,
         metadata=ToolMetadata(
-            name="croatia_airlines_2022",
+            name="croatia_airlines_report_2022",
             description=(
-                "Provides information about Croatia Airlines financials for year 2021. "
+                "Provides information about Croatia Airlines bussines report for year 2021. "
                 "Use a detailed plain text question as input to the tool."
             ),
         ),
@@ -95,7 +97,19 @@ query_engine_tools = [
 
 # kreiraj agenta i proslijedi alate
 from llama_index.agent import OpenAIAgent
-agent = OpenAIAgent.from_tools(query_engine_tools, verbose=True)
+agent = OpenAIAgent.from_tools(
+    query_engine_tools, 
+    verbose=True,
+    system_prompt="""
+        - The questions are for Croatia Airlines report for years 2021 and 2022. 
+        - Remember to always use available tools
+        - explain how did you get the final answer 
+    """
+)
 
-# razgovaraj sa agentom
-agent.chat_repl()
+# postavi pojedinačno pitanje
+res = agent.query("which year had more employes")
+print(res)
+
+# # razgovaraj sa agentom
+# agent.chat_repl()
